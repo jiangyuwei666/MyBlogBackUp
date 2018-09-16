95f7666e12ecc014b760a437d12b457ea7de479f
jiangyuwei666 <739843128@qq.com>
2018-09-16 14:53:28 +0800

    copy

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://litten.me/ins-min/' + data.link[i] + '.min.jpg';
-          var src = 'http://litten.me/ins/' + data.link[i];
+          var minSrc = 'https://github.com/jiangyuwei666/MyBlogBackUp/blob/master/min_photos/' + data.link[i] + '.min.jpg';
+          var src = 'https://github.com/jiangyuwei666/MyBlogBackUp/blob/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '.jpg';
